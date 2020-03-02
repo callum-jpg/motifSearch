31e7d29840d609c7dcaf873c445e11cd15361d0d
callum-jpg <23258116+callum-jpg@users.noreply.github.com>
2020-03-02 17:33:05 +0000

    Used Cartesian product to determine IUPAC to DNA possible combinations
    After comparing different ways to convert IUPAC ambiguous DNA sequence into
    all possible DNA sequences in list format, product from itertools was used.
    This was added into functions.py and old functions were removed.
    Also tidied code

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-from script import IUPAC_dict
+from itertools import product
 
 def percentage(percent, whole):
     '''Calculates the percentage with float#
@@ -8,26 +8,24 @@ def percentage(percent, whole):
     return ((percent/float(whole)) * 100.0)
 
 
-def num_combinations(motif):
-    """Returns the number of possible combinations given a motif in
-    IUPAC format
-
-    >>> num_combinations('TNTC')
-    >>> 4
-
-    >>> num_combinations('TNNN')
-    >>> 64
+def iupac_to_dna(sequence):
     """
-    combi = 1
-    for i in motif:
-        combi = combi * len(IUPAC_dict[i])
-    return combi
+    Takes an input DNA sequence in IUPAC format and returns all possible sequences.
 
-def max_iupac(motif):
-    """
-    Returns the maximum number of nucleotides that an IUPAC denotes
+    >>> iupac_to_dna('TN')
+    >>> ['TA', 'TT', 'TG', 'TC']
 
-    >>> max_iupac('N')
-    >>> 4
+    >>> iupac_to_dna('TNTM')
+    >>> ['TATA', 'TATC', 'TTTA', 'TTTC', 'TGTA', 'TGTC', 'TCTA', 'TCTC']
     """
-    return max([len(IUPAC_dict[i]) for i in motif])
+    # Cartesian product will generate all of the possible ordered motif strings from ambiguous IUPAC nucleotides
+    # Product example: product(A, B) returns the same as ((x,y) for x in A for y in B)
+
+    # [IUPAC_dict[j] for j in tmotif] creates an ordered 2d array (ordered, as in, the same order as the input motif)
+    # of all possible bases that occur at each position of the sequence
+
+    # The asterix (*) before IUPAC_dict is used to unpack the argument list into individual lists
+
+    # "".join(i) takes each cartesian product ('x', 'y', 'z') from the cartesian output list
+    # and collapses it into a single string ('xyz') on a item by item bases (hence the for i...)
+    return ["".join(i) for i in product(*[IUPAC_dict[j] for j in sequence])]
--- motifSearch.py
+++ motifSearch.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python
 import functions as fc
-from script import IUPAC_dict
 import wget
-from Bio.Seq import Seq
+from Bio import Seq
 from Bio import SeqIO
 from Bio.Alphabet import IUPAC
 import pandas as pd
 import numpy as np
 
-fc.num_combinations('TN')
+
+IUPAC_dict = Seq.IUPAC.IUPACData.ambiguous_dna_values
 fc.percentage(50, 100)
-fc.max_iupac('N')
+fc.iupac_to_dna('TNTM')
 
 # Ensembl GRCh38 human genome. Chromosome 22 only (for faster testing)
 wget.download('http://genomedata.org/rnaseq-tutorial/fasta/GRCh38/chr22_with_ERCC92.fa')
 # Download E. coli K12 gDNA
 wget.download('ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz')
-
 
 input_file = 'GCF_000005845.2_ASM584v2_genomic.fna'
 
@@ -26,73 +25,15 @@ input_file = 'GCF_000005845.2_ASM584v2_genomic.fna'
 # Record.seq is the sequence of that record
 
 for record in SeqIO.parse(input_file, 'fasta'):
-    print("Record " + record.id + ", length " + str(len(record.seq)) +
-          "First/last 10 bases: " + record.seq[:10] + "..." + record.seq[-10:])
+    print("Record " + record.id + ", length " + str(len(record.seq)))
 
-# Returns record referring to chromosomes, Y, and mitochondrial DNA
+# Returns record referring to 23 chromosomes, Y, and mitochondrial DNA
 for record in SeqIO.parse('GRCh38_latest_genomic.fna', 'fasta'):
     if 'NC_' in record.id:
         print record.id
 
-
-## Counting motifs
-# This is inaccuate - only counts TTTC motifs
-for record in SeqIO.parse(input_file, 'fasta', alphabet = IUPAC.unambiguous_dna):
-    sequence = record.seq
-    rev_sequence = record.seq.reverse_complement()
-    ## Sequence.count must be converted to string for print
-    print(record.seq.alphabet)
-    print("Record " + record.id + " has a sequence length of " + str(len(record.seq)) +
-          "bp. \nIt contains " +
-          str(sequence.count('TTTC' or
-          'TGTC' or
-          'TCTC' or
-          'TATC')) + " TNTC motifs and " +
-          str(rev_sequence.count('TTTC' or
-          'TGTC' or
-          'TCTC' or
-          'TATC')) + " TNTC motifs on the reverse complement.")
-
-## Counting occurence of variations of TNTC motifs
-for record in SeqIO.parse(input_file, 'fasta'):
-    sequence = record.seq
-    rev_sequence = record.seq.reverse_complement()
-    modified = (rev_sequence.count('TTTC') +
-                sequence.count('TGTC') +
-                sequence.count('TCTC') +
-                sequence.count('TATC') +
-                rev_sequence.count('TTTC') +
-                rev_sequence.count('TGTC') +
-                rev_sequence.count('TCTC') +
-                rev_sequence.count('TATC'))
-
-    ## Sequence.count must be converted to string for print
-    print("Record " + record.id + " is " + str(len(record.seq)) + "bp long.\n"
-          " It contains " +
-          str(sequence.count('TTTC')) + ' TTTC motifs, ' +
-          str(sequence.count('TGTC')) + ' TGTC motifs, ' +
-          str(sequence.count('TCTC')) + ' TCTC motifs, and ' +
-          str(sequence.count('TATC')) + ' TATC motifs.\n' +
-          str(rev_sequence.count('TTTC')) + ' TTTC motifs, ' +
-          str(rev_sequence.count('TGTC')) + ' TGTC motifs, ' +
-          str(rev_sequence.count('TCTC')) + ' TCTC motifs, and ' +
-          str(rev_sequence.count('TATC')) + ' TATC motifs on the reverse complement')
-
-    # 1.5% of E. coli K12 genome can be modified by Taq DarT
-    ((modified / float(len(sequence) * 2)) * 100)
-
-
-
-
-
-# Motif search test
-test_seq = Seq("ATAGCTCTAGCTATGCTACGATACGTGTC")
-test_seq2 = Seq("TGTCTGTCTGTC")
-
-
-
 motif_data = {}
-motifs = ['TTTC', 'TGTC', 'TCTC', 'TATC']
+motifs = fc.iupac_to_dna('TNTC')
 for record in SeqIO.parse(input_file, 'fasta'):
     sequence = record.seq
     rev_sequence = record.seq.reverse_complement()
@@ -112,111 +53,13 @@ for record in SeqIO.parse(input_file, 'fasta'):
     motif_data["Total reverse complement sites"] = total_sites_rev
     motif_data["Total number of sites"] = total_sites + total_sites_rev
 
-
-
 # Data dict output
 output = pd.DataFrame(data=motif_data, index=[0])
 output.to_csv('output.csv')
 
-
 t = {}
 t = {'record' : ['bacterial gDNA', 'human gDNA'], motifs[0] : [1, 200]}
 pd.DataFrame(data=t)
-
-IUPAC = {'A': 'A',
-         'T': 'T',
-         'G': 'G',
-         'C': 'C',
-         'N': ['A', 'T', 'G', 'C'],
-         'R': ['A', 'G'],
-         'Y': ['C', 'T'],
-         'S': ['G', 'C'],
-         'W': ['A', 'T'],
-         'K': ['G', 'T'],
-         'M': ['A', 'C'],
-         'B': ['C', 'G', 'T'],
-         'D': ['A', 'G', 'T'],
-         'H': ['A', 'C', 'T'],
-         'V': ['A', 'C', 'G']
-         }
-
-def iupac_motif(motif_string):
-    """
-    Takes a motif in IUPAC format and generates a list of possible strings
-
-    >>> IUPAC_motif("TNTC")
-    >>> ['TTTC', 'TATC', 'TGTC', 'TCTC']
-    """
-
-
-
-
-
-# Find associated values with key
-IUPAC['N']
-# Find length of values for key 'N' - decides how many motifs to generate
-len(IUPAC['N'])
-# Print values for a specific key
-for i in IUPAC['N']:
-    print i
-# Generate unique motifs
-
-### Calculate  possible motifs for TN (4 total):
-
-# First, calculate possible combinations:
-fc.num_combinations('TBV')
-fc.max_iupac('TBV')
-
-tmotif = 'TN'
-tlist = []
-for i in range(0, fc.num_combinations(tmotif)):
-    for j in range(1, fc.max_iupac(tmotif)):
-        print j
-        tlist.append(IUPAC_dict[tmotif[0]] + IUPAC_dict[tmotif[1]])
-
-
-# If using append, generate the full motif within the append function
-# This will require the IUPAC_dict.value position
-# This will require any values below max(IUPAC_dict.values()) to be repeated
-tlist.append("T" + "T")
-
-
-# np.empty creates shape but doesn't use zeros - uses arbitrary data
-# Scrapping the idea of saving motifs in an array - will instead store in a list
-# For visualisation of all motifs, list could be coverted to array later.
-np.empty((fc.num_combinations('TBV')/fc.max_iupac('TBV'), fc.max_iupac('TBV')))
-
-np.empty((10, 10))
-
-
-
-
-for i in 'TN':
-    if len(IUPAC_dict[i]) > 1:
-        print IUPAC_dict[i]
-
-max([len(IUPAC_dict[i]) for i in 'TN'])
-
-
-
-
-max([1, 2, 3, 4])
-
-examp = (['TTTC', 'TATC', 'TCTC', 'TGTC'])
-a = np.array([[1, 2, 3], [4, 5, 6]])
-# i, j[start:end, start:end]
-a[:2, 1:3]
-a
-# Calculate possible motifs for TNN (16 total):
-
-a
-# i, j[start:end, start:end]
-a[:2, 1:3]
-# Pull 0th column
-a[:, 0]
-# Pull index 1 row
-a[1, :]
-
 
 
 
