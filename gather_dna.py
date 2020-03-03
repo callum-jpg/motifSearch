c398741a5ddba71f2766b6e231f8c572ede58f55
callum-jpg <23258116+callum-jpg@users.noreply.github.com>
2020-03-03 15:39:28 +0000

    Added script for downloading and processing gDNA fasta files

--- gather_dna.py
+++ gather_dna.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 import wget
+from Bio import SeqIO
 
 # Ensembl GRCh38 human genome. Chromosome 22 only (for faster testing)
 wget.download('http://genomedata.org/rnaseq-tutorial/fasta/GRCh38/chr22_with_ERCC92.fa')
 # Download E. coli K12 gDNA
 wget.download('ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz')
+
+
+# From human gDNA, move chr1:23 + Y into separate fasta
+input_seq_iterator = SeqIO.parse('GRCh38_latest_genomic.fna', 'fasta')
+chromosomes = [record for record in input_seq_iterator
+               if 'NC_00' in record.id]
+SeqIO.write(chromosomes, "chr1-23+Y.fna", "fasta")
