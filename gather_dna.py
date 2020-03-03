b725edfc4a62aded3c8290ded401eb3728e4fa7b
callum-jpg <23258116+callum-jpg@users.noreply.github.com>
2020-03-03 14:30:38 +0000

    Initial commit. Script down downloading gDNA

---
+++ gather_dna.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+import wget
+
+# Ensembl GRCh38 human genome. Chromosome 22 only (for faster testing)
+wget.download('http://genomedata.org/rnaseq-tutorial/fasta/GRCh38/chr22_with_ERCC92.fa')
+# Download E. coli K12 gDNA
+wget.download('ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz')
