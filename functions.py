6696731baf2d852ea816349ee4c5fe0babf4d933
callum-jpg <23258116+callum-jpg@users.noreply.github.com>
2020-03-03 14:24:30 +0000

    added motif read fasta feature function

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 from itertools import product
 from Bio import Seq
+from Bio import SeqIO
 
 IUPAC_dict = Seq.IUPAC.IUPACData.ambiguous_dna_values
 
@@ -32,3 +33,46 @@ def iupac_to_dna(sequence):
     # "".join(i) takes each cartesian product ('x', 'y', 'z') from the cartesian output list
     # and collapses it into a single string ('xyz') on a item by item bases (hence the for i...)
     return ["".join(i) for i in product(*[IUPAC_dict[j] for j in sequence])]
+
+
+def motifs_in_fasta(fasta, motif):
+    """Returns a dictionary of the number of motifs found within each FASTA sequence identified.
+    Parses through all available FASTA sequences. Identifies by name after >.
+    Identifies motifs on both complement and reverse complement.
+
+    fasta is a file in FASTA format
+    motif is a short sequence in IUPAC format
+    """
+    motif_data = {} # Create empty dictionary to hold motif counts
+    motif_variations = iupac_to_dna(motif)
+
+    # Create empty lists in dict to store values
+    output_cols = ["Record",
+                   "Total complement sites",
+                   "Total reverse complement sites",
+                   "Total number of sites",
+                   "Perc DNA modified (total)"]
+    # Populate dict with empty lists
+    for i in output_cols:
+        motif_data[i] = []
+
+    for record in SeqIO.parse(fasta, 'fasta'):
+        sequence = record.seq # Read the sequence associated with record
+        rev_sequence = record.seq.reverse_complement() # Get the reverse complement
+        total_sites = 0 # Counter for complement motifs
+        total_sites_rev = 0 # counter for rev complement motifs
+
+        for j in range(0, len(motif_variations)): # For every motif, count the number of occurrences
+            total_sites += sequence.count(motif_variations[j])
+            total_sites_rev += rev_sequence.count(motif_variations[j])
+
+        motif_data[output_cols[0]].append(record.id) # Create a new dict entry to hold FASTA record name
+
+        # Add counts to dict
+        motif_data[output_cols[1]].append(total_sites)
+        motif_data[output_cols[2]].append(total_sites_rev)
+        motif_data[output_cols[3]].append((total_sites + total_sites_rev))
+        motif_data[output_cols[4]].append(percentage((total_sites + total_sites_rev),
+                                                        (len(sequence) + len(rev_sequence))))
+
+    return motif_data
--- motifSearch.py
+++ motifSearch.py
@@ -9,13 +9,19 @@ import pandas as pd
 IUPAC_dict = Seq.IUPAC.IUPACData.ambiguous_dna_values
 fc.percentage(50, 100)
 fc.iupac_to_dna('TNTM')
+fc.motifs_in_fasta(input_file, 'TNTC')
+
 
 # Ensembl GRCh38 human genome. Chromosome 22 only (for faster testing)
 wget.download('http://genomedata.org/rnaseq-tutorial/fasta/GRCh38/chr22_with_ERCC92.fa')
 # Download E. coli K12 gDNA
 wget.download('ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz')
 
-input_file = 'GCF_000005845.2_ASM584v2_genomic.fna'
+input_file = 'test-seq.fna'
+
+fc.motifs_in_fasta(input_file, 'TNTC')
+output = pd.DataFrame(data=motifs_in_fasta(input_file, 'TNTC'))
+output.to_csv('output.csv')
 
 # SeqIO.parse creates a 'SeqRecord' object takes the record identifier '>' and returns
 # information for that record, such as sequence length and any other information.
@@ -29,38 +35,3 @@ for record in SeqIO.parse(input_file, 'fasta'):
 for record in SeqIO.parse('GRCh38_latest_genomic.fna', 'fasta'):
     if 'NC_' in record.id:
         print record.id
-
-motif_data = {}
-motifs = fc.iupac_to_dna('TNTC')
-for record in SeqIO.parse(input_file, 'fasta'):
-    sequence = record.seq
-    rev_sequence = record.seq.reverse_complement()
-    total_sites= 0
-    total_sites_rev = 0
-    motif_data['Record'] = record.id
-    for i in range(0, len(motifs)):
-        # Build up total number of sites over motif interations
-        total_sites += sequence.count(motifs[i])
-        total_sites_rev += rev_sequence.count(motifs[i])
-        # Add individual counts for each motif to data dict
-        motif_data[motifs[i] + " complement sites"] = sequence.count(motifs[i])
-        motif_data[motifs[i] + " reverse complement sites"] = rev_sequence.count(motifs[i])
-
-    # At end of motif search, build dictionary with total sites
-    motif_data["Total complement sites"] = total_sites
-    motif_data["Total reverse complement sites"] = total_sites_rev
-    motif_data["Total number of sites"] = total_sites + total_sites_rev
-
-# Data dict output
-output = pd.DataFrame(data=motif_data, index=[0])
-output.to_csv('output.csv')
-
-t = {}
-t = {'record' : ['bacterial gDNA', 'human gDNA'], motifs[0] : [1, 200]}
-pd.DataFrame(data=t)
-
-
-
-
-
-
