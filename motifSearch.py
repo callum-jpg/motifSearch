0071b5c09737dffca70b9ec2856f1a8c1839dd8c
callum-jpg <23258116+callum-jpg@users.noreply.github.com>
2020-06-03 16:00:32 +0100

    pretty motif % gDNA plots

--- motifSearch.py
+++ motifSearch.py
@@ -45,29 +45,47 @@ output.to_csv('output.csv')
 # Bacterial gDNA
 mots = ['TNTC', 'TNTM', 'TYT', 'TTTH']
 input_data = pd.read_csv('output.csv')
+# Colours for plotting. From Solarized
+colour_palette = [(211, 54, 130), (108, 113, 196), (38, 139, 210), (42, 161, 152)]
+plot_colours = colour_palette
+for i in range(len(colour_palette)):
+	r, g, b = colour_palette[i]
+	# Convert RGB (0, 255) to (0, 1) which matplotlib likes
+	plot_colours[i] = (r / 255, g / 255, b / 255)
 
-fig, axes = plt.subplots(nrows=2, ncols=2)
+
+fig, axes = plt.subplots(nrows=2, ncols=2, sharey=True, sharex=True)
 for i, ax in enumerate(axes.flatten()):
 	subset = input_data[input_data['motif seq'] == str(mots[i])]
-	ax.bar(subset['Record'], subset['Perc DNA modified (total)'])
-	ax.set_ylim([0, 3])
+	ax.bar(subset['Record'], subset['Perc DNA modified (total)'], color=plot_colours[i])
 	ax.set_xticklabels(labels=subset['Record'], rotation=45, ha="right", rotation_mode="anchor")
 	ax.set_title('\'{0}\' motif'.format(mots[i]))
 	# Remove plot borders
 	ax.spines["top"].set_visible(False)
-	ax.spines["bottom"].set_visible(False)
+	#ax.spines["bottom"].set_visible(False)
 	ax.spines["right"].set_visible(False)
-	ax.spines["left"].set_visible(False)
+	#ax.spines["left"].set_visible(False)
 	# Tick marks
 	ax.get_yaxis().tick_left()
 	ax.tick_params(axis="both", which="both", bottom=False, left=False)
 	# Display yticks with intervals of 1. Alternative oneliner: ax.set_yticks(ax.get_yticks()[::2])
+	ax.set_ylim([0, 3])
 	ymin, ymax = ax.get_ylim()
 	ax.set_yticklabels(np.arange(ymin, ymax+1, 1, dtype=np.int))
-	ax.set_ylabel('% gDNA modified')
-fig.suptitle('This is the figure title')
-fig.tight_layout(pad=0.1, rect=[0, 0.03, 1, 0.9])
-fig.savefig("some.png", dpi=300)
+	#ax.set_ylabel('% gDNA modified')
+# fig.suptitle('This is the figure title')
+fig.text(0.01, 0.5, '% gDNA modified', va='center', rotation='vertical') # Common Y axis label
+#plt.setp(axes[:, 0], ylabel='% gDNA modified') # Apply yaxis label only to plots in column 1
+fig.tight_layout(rect=[0, 0.03, 1, 0.9]) # Call tight_layout last
+
+
+matplotlib.use("TkAgg") # Backend to use for PyCharm interactive plots
+matplotlib.use("GTK3Agg") # Backend to use for savefig with properly scaled DPI
+fig.savefig("plots/colours - common y label.png", dpi=300)
+
+
+
+
 
 
 # ONE PLOT ONLY
