2ec1d39285ed0b8ca105499d019a521143176d44
callum-jpg <23258116+callum-jpg@users.noreply.github.com>
2020-09-09 17:11:26 +0100

    Tidied up the motif-plots and gather_dna tests
    Following a switch from PyCharm to Spyder, I decided to clean up some of my
    plotting code. Spyder, since it's based on IPython, makes plotting graphs etc.
    much easier than PyCharm. Cells are delineated with '#%%'. Moreover,
    I tidied up some of the plotting code to remove now redundant code.

--- tests/gather_dna.py
+++ tests/gather_dna.py
@@ -1,15 +1,10 @@
-#!/usr/bin/env python
-import wget
+#!/usr/bin/env python3
+
 from Bio import SeqIO
-import functions as fc
-
-# For realoding
-import importlib
-importlib.reload(fc)
+from motifsearch import countmotifs as ms
 
 
-
-
+#%% Downloading bacterial gDNA fasta sequences
 
 # Bacterial gDNA of interest with RefSeq sequence
 # Gather accession numbers from NCBI 'assembly' database
@@ -22,84 +17,26 @@ bac_gdna_seqs = {
     'P.aeruginosa': 'AE004091.2'
 }
 
+
 # Download a dictionary of dna
 for k, v in bac_gdna_seqs.items():
     filename = k + '-gdna.fa'
-    fc.download_dna(v, filename)
-
-bac_gdna_seq_extra = {
-    **bac_gdna_seqs, # ** unpacks dict and adds key: value pairs and merges to dictionary
-    'D.compactum': 'NZ_AP018316.1',
-    'S.enterica': 'NC_003197.2',
-    'G.thermoleovorans': 'NZ_CP014335.1',
-    'B.naejangsanensis': 'NZ_CP015614.1',
-    'B.cereus': 'NZ_CP034551.1',
-    'C.abyssi': 'NZ_CP018099.1',
-    'B.diminuta': 'NZ_CP035093.1',
-    'V.cholerae': 'NZ_CP010812.1',
-    'N.canis': 'NZ_LR134313.1',
-    'T.scotoductus': 'NC_014974.1',
-    'G.violaceus': 'NC_005125.1',
-    'S.cyanosphaera': 'NC_019748.1',
-    'L.trevisanii': 'NZ_AP019840.1',
-    'A.giovannonii': 'NZ_CP042997.1',
-    'P.bacterium': 'NZ_CP036264.1',
-    'S.cyanobacteriorum': 'NZ_CP019633.1',
-    'P.mikurensis': 'NC_017080.1',
-    'R.baltica': 'NC_005027.1',
-    'L.pneumophila': 'NZ_LR134380.1'
-}
-#len(bac_gdna_seq_extra)
-
-# Pathogens
-# 'A.baumannii': 'NZ_CP046654.1',
-# 'K.pneumoniae': 'NC_016845.1',
-# 'E.hormaechei': 'NZ_CP017179.1'
-
-# Download a dictionary of dna
-for k, v in bac_gdna_seq_extra.items():
-    filename = k + '-gdna.fa'
-    fc.download_dna(v, filename)
-
-
+    ms.download_dna(v, filename)
+    
+    
 # Test for concatenate_fasta
-fc.concatenate_fasta('downloaded_DNA', 'all-bac-seq.fa')
+ms.concatenate_fasta('downloaded_DNA', 'all-bac-seq.fa')
 
-fc.concatenate_fasta('downloaded_DNA', 'all-bac-seq-extra.fa')
+# Rename fasta sequences based on their key in bac_gdna_seqs
+ms.rename_fasta_id(bac_gdna_seqs, 'downloaded_DNA/all-bac-seq.fa', 'downloaded_DNA/all-bac-renamed.fa')
+
+#%% Checking the contents of the concatenated fasta file.
+
+ms.fasta_record_check('downloaded_DNA/all-bac-seq.fa')
+ms.fasta_record_check('downloaded_DNA/all-bac-renamed.fa')
 
 
-
-# Test for rename_fasta_id
-bac_gdna_seqs = {
-    'T.aquaticus': 'NZ_CP010822.1',
-    'EPEC': 'LT827011.1',
-    'E.coli(K12)': 'NC_000913.3',
-    'M.tuberculosis': 'NC_000962.3',
-    'P.aeruginosa': 'AE004091.2'
-}
-fc.rename_fasta_id(bac_gdna_seqs, 'downloaded_DNA/all-bac-seq.fa', 'downloaded_DNA/all-bac-renamed.fa')
-
-fc.rename_fasta_id(bac_gdna_seq_extra, 'downloaded_DNA/all-bac-seq-extra.fa', 'downloaded_DNA/all-bac-extra-renamed.fa')
-
-fc.fasta_record_check('downloaded_DNA/all-bac-seq.fa')
-fc.fasta_record_check('downloaded_DNA/all-bac-renamed.fa')
-
-fc.fasta_record_check('downloaded_DNA/all-bac-seq-extra.fa')
-fc.fasta_record_check('downloaded_DNA/all-bac-extra-renamed.fa')
-
-# Human gDNA
-## OLD CODE ##
-
-# Ensembl GRCh38 human genome. Chromosome 22 only (for faster testing)
-wget.download('http://genomedata.org/rnaseq-tutorial/fasta/GRCh38/chr22_with_ERCC92.fa')
-
-# From human gDNA, move chr1:23 + Y into separate fasta
-input_seq_iterator = SeqIO.parse('GRCh38_latest_genomic.fna', 'fasta')
-chromosomes = [record for record in input_seq_iterator
-               if 'NC_00' in record.id]
-SeqIO.write(chromosomes, "chr1-23+Y.fna", "fasta")
-
-fc.fasta_record_check('chr1-23+Y.fna')
+#%% Downloading human gDNA - autosomes only
 
 human_chr = {
     "chr1": "NC_000001.11",
@@ -124,24 +61,31 @@ human_chr = {
     "chr20": "NC_000020.11",
     "chr21": "NC_000021.9",
     "chr22": "NC_000022.11",
-    "chrX": "NC_000023.11",
-    "chrY": "NC_000024.10"
 }
 
-fc.rename_fasta_id(human_chr, 'chr1-23+Y.fna', "downloaded_DNA/human_gdna_renamed.fa")
+for k, v in human_chr.items():
+    filename = k + '-gdna.fa'
+    ms.download_dna(v, filename)
+    
 
-fc.fasta_record_check('downloaded_DNA/human_gdna_renamed.fa')
+ms.concatenate_fasta('downloaded_DNA/human-gdna', '1-22-hs-gdna.fa')
+
+ms.rename_fasta_id(human_chr, 'downloaded_DNA/human-gdna/1-22-hs-gdna.fa', 
+                   'downloaded_DNA/human-gdna/1-22-hs-gdna-renamed.fa')
+
+#%% Checking the contents of the concatenated fasta file.
+
+ms.fasta_record_check('downloaded_DNA/human-gdna/1-22-hs-gdna.fa')
+
+ms.fasta_record_check('downloaded_DNA/human-gdna/1-22-hs-gdna-renamed.fa')
 
 
-# # Extract just the gDNA based on record.id
-# def extract_gdna(input_file, gdna_record, output_file):
-#     """Takes an input FASTA file, extracts the desired gDNA containing
-#     record and outputs as a new FASTA file"""
-#     input_iter = SeqIO.parse(input_file, 'fasta')
-#     gdna = [record for record in input_iter
-#             if gdna_record in record.id]
-#     SeqIO.write(gdna, output_file, 'fasta')
-#
-# extract_gdna('GCF_001399775.1_ASM139977v1_genomic.fna', 'NZ_CP010822', 'taq-gdna.fna')
-#
+
+
+
+
+
+
+
+
 
--- tests/motif-plots.py
+++ tests/motif-plots.py
@@ -1,41 +1,15 @@
-#!/usr/bin/env python
-import matplotlib
-matplotlib.use("TkAgg") # Backend to use for PyCharm interactive plots
-matplotlib.use("GTK3Agg") # Backend to use for savefig with properly scaled DPI
+#!/usr/bin/env python3
 
-import functions as fc
+#%% Import packages
+
+from motifsearch import countmotifs as ms
 from Bio import SeqIO
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 import numpy as np
+from sklearn.linear_model import LinearRegression
 
-# Test plot viewer is working
-plt.plot([1,2,3])
-plt.show()
-
-# For realoding
-import importlib
-importlib.reload(fc)
-
-#fc.percentage(50, 100)
-#fc.iupac_to_dna('TNTM')
-
-
-# Bacterial gDNA
-# Calculating data before loop
-mots = ['TNTC', 'TNTM', 'TYT', 'TTTH']
-output = pd.DataFrame()
-for i, _ in enumerate(mots):
-	print("Counting sites for {0} motif".format(mots[i]))
-	df = pd.DataFrame(data=fc.motifs_in_fasta('downloaded_DNA/all-bac-renamed.fa', str(mots[i])))
-	output = output.append(df)
-output.to_csv('output.csv')
-
-
-# Bacterial gDNA
-mots = ['TNTC', 'TNTM', 'TYT', 'TTTH']
-input_data = pd.read_csv('output.csv')
+#%% Create RGB colours for plotting
 
 # Colours for plotting. From Solarized palette
 colour_palette = [(211, 54, 130), (108, 113, 196), (38, 139, 210), (42, 161, 152)]
@@ -45,6 +19,48 @@ for i in range(len(colour_palette)):
 	# Convert RGB (0, 255) to (0, 1) which matplotlib likes
 	plot_colours[i] = (r / 255, g / 255, b / 255)
 
+#%%
+
+
+# Bacterial gDNA
+# Calculating data before plot loop
+# Assumes DNA has already been downloaded 
+mots = ['TNTC', 'TNTM', 'TYT', 'TTTH']
+output = pd.DataFrame()
+for i, _ in enumerate(mots):
+	print("Counting sites for {0} motif".format(mots[i]))
+	df = pd.DataFrame(data=ms.motifs_in_fasta('tests/downloaded_DNA/all-bac-renamed.fa', str(mots[i])))
+	output = output.append(df)
+output.to_csv('output.csv')
+
+
+#%% Plot gDNA lengths
+
+input_data = pd.read_csv('output.csv')
+# Subset to take TNTC only (motif doesn't matter, just don't want to repeatedly plot the same gDNA length)
+subset = input_data[input_data['motif seq'] == 'TNTC']
+
+fig, ax = plt.subplots()
+ax.bar(subset['Record'], subset['record length'], color=plot_colours[1])
+ax.set_title('gDNA length between species')
+ax.set_xticklabels(labels=subset['Record'], rotation=45, ha="right", rotation_mode="anchor")
+ax.spines["top"].set_visible(False)
+ax.spines["right"].set_visible(False)
+ax.get_yaxis().tick_left()
+ax.tick_params(axis="both", which="both", bottom=False, left=False)
+# Display yticks with intervals of 1. Alternative oneliner: ax.set_yticks(ax.get_yticks()[::2])
+ax.set_ylim([0, 7e6])
+ymin, ymax = ax.get_ylim()
+ax.set_yticklabels(np.arange(ymin, ymax + 1e6, 1e6, dtype=np.int) / 1e6) #  Convert to Mb
+ax.set_ylabel('gDNA length (Mb)')
+fig.tight_layout(rect=[0, 0.03, 1, 0.9]) # Call tight_layout last
+
+fig.savefig("tests/gDNA length between species.png", dpi=300)
+
+
+#%% Plot output.csv for different motifs occurance in bacterial gDNA
+
+input_data = pd.read_csv('output.csv')
 
 fig, axes = plt.subplots(nrows=2, ncols=2, sharey=False, sharex=True)
 for i, ax in enumerate(axes.flatten()):
@@ -71,52 +87,11 @@ fig.text(0.01, 0.5, '% gDNA modified', va='center', rotation='vertical') # Commo
 fig.tight_layout(rect=[0, 0.03, 1, 0.9]) # Call tight_layout last
 
 
-matplotlib.use("TkAgg") # Backend to use for PyCharm interactive plots
-matplotlib.use("GTK3Agg") # Backend to use for savefig with properly scaled DPI
-fig.savefig("plots/colours - common x+y - all numbers presented.png", dpi=300)
+fig.savefig("tests/colours - common x+y - all numbers presented.png", dpi=300)
 
+#%% Plot gDNA length vs % DNA modified
 
-# ONE PLOT ONLY
-input_data = pd.read_csv('output.csv')
-subset = input_data[input_data['motif seq'] == 'TNTC']
-#subset['Record']
-#subset['Perc DNA modified (total)']
-plt.bar(subset['Record'], subset['Perc DNA modified (total)'])
-
-
-# Bar plot of gDNA lengths
-input_data = pd.read_csv('output.csv')
-subset = input_data[input_data['motif seq'] == 'TNTC']
-
-fig, ax = plt.subplots()
-ax.bar(subset['Record'], subset['record length'], color=plot_colours[1])
-ax.set_title('gDNA length between species')
-ax.set_xticklabels(labels=subset['Record'], rotation=45, ha="right", rotation_mode="anchor")
-ax.spines["top"].set_visible(False)
-ax.spines["right"].set_visible(False)
-ax.get_yaxis().tick_left()
-ax.tick_params(axis="both", which="both", bottom=False, left=False)
-# Display yticks with intervals of 1. Alternative oneliner: ax.set_yticks(ax.get_yticks()[::2])
-ax.set_ylim([0, 7e6])
-ymin, ymax = ax.get_ylim()
-ax.set_yticklabels(np.arange(ymin, ymax + 1e6, 1e6, dtype=np.int) / 1e6)
-ax.set_ylabel('gDNA length (Mb)')
-fig.tight_layout(rect=[0, 0.03, 1, 0.9]) # Call tight_layout last
-
-matplotlib.use("TkAgg") # Backend to use for PyCharm interactive plots
-matplotlib.use("GTK3Agg") # Backend to use for savefig with properly scaled DPI
-fig.savefig("plots/gDNA length between species.png", dpi=300)
-
-
-# Plot % gDNA modified vs gDNA length
-# Single plot for a single motif. Record length vs % gDNA modified
-input_data = pd.read_csv('output.csv')
-subset = input_data[input_data['motif seq'] == 'TNTC']
-
-from sklearn.linear_model import LinearRegression
-
-
-linear_reg = LinearRegression()
+linear_reg = LinearRegression() 
 fig, axes = plt.subplots(nrows=2, ncols=2, sharey=False, sharex=True)
 for i, ax in enumerate(axes.flatten()):
 	subset = input_data[input_data['motif seq'] == str(mots[i])]
@@ -147,32 +122,26 @@ fig.text(0.5, 0.04, 'gDNA length (Mb)', ha='center') # Common x axis label
 fig.text(0.01, 0.5, '% gDNA modified', va='center', rotation='vertical')  # Common Y axis label
 fig.tight_layout(rect=[0.02, 0.05, 1, 0.9]) # Call tight_layout last
 
-matplotlib.use("TkAgg") # Backend to use for PyCharm interactive plots
-matplotlib.use("GTK3Agg") # Backend to use for savefig with properly scaled DPI
-fig.savefig("plots/gDNA length vs % gDNA modified.png", dpi=300)
+fig.savefig("tests/gDNA length vs % gDNA modified.png", dpi=300)
+
+
+#%% Human gDNA plotting
 
 
 
-### Human gDNA/motif plots ###
+
 mots = ['TNTC', 'TNTM', 'TYT', 'TTTH']
 output = pd.DataFrame()
 for i, _ in enumerate(mots):
 	print("Counting sites for {0} motif".format(mots[i]))
-	df = pd.DataFrame(data=fc.motifs_in_fasta('downloaded_DNA/human_gdna_renamed.fa', str(mots[i])))
+	df = pd.DataFrame(data=ms.motifs_in_fasta('downloaded_DNA/human-gdna/1-22-hs-gdna-renamed.fa', str(mots[i])))
 	output = output.append(df)
 output.to_csv('output_human_gdna.csv')
 
-# Plotting
+#%% Plot percentage of gDNA modified for all motifs
 
-# Colours for plotting. From Solarized palette
-colour_palette = [(211, 54, 130), (108, 113, 196), (38, 139, 210), (42, 161, 152)]
-plot_colours = colour_palette
-for i in range(len(colour_palette)):
-	r, g, b = colour_palette[i]
-	# Convert RGB (0, 255) to (0, 1) which matplotlib likes
-	plot_colours[i] = (r / 255, g / 255, b / 255)
-
-input_data = pd.read_csv("output_human_gdna.csv")
+mots = ['TNTC', 'TNTM', 'TYT', 'TTTH']
+input_data = pd.read_csv('output_human_gdna.csv')
 
 fig, axes = plt.subplots(nrows=2, ncols=2, sharey=False, sharex=True, figsize=(12, 5))
 for i, ax in enumerate(axes.flatten()):
@@ -189,7 +158,7 @@ for i, ax in enumerate(axes.flatten()):
 	ax.get_yaxis().tick_left()
 	ax.tick_params(axis="both", which="both", bottom=False, left=False)
 	# Display yticks with intervals of 1. Alternative oneliner: ax.set_yticks(ax.get_yticks()[::2])
-	ax.set_ylim([0, 3])
+	ax.set_ylim([0, 5])
 	ymin, ymax = ax.get_ylim()
 	ax.set_yticklabels(np.arange(ymin, ymax+1, 1, dtype=np.int))
 	#ax.set_ylabel('% gDNA modified')
@@ -199,13 +168,13 @@ fig.text(0.0001, 0.5, '% gDNA modified', va='center', rotation='vertical') # Com
 fig.tight_layout(rect=[0, 0.03, 1, 0.9]) # Call tight_layout last. (left, bottom, right, top)
 
 
-matplotlib.use("TkAgg") # Backend to use for PyCharm interactive plots
-matplotlib.use("GTK3Agg") # Backend to use for savefig with properly scaled DPI
-fig.savefig("plots/human gDNA - all motifs", dpi=300)
+fig.savefig("tests/human gDNA - all motifs", dpi=300)
 
 
-# Single human gDNA TNTC plot
-input_data = pd.read_csv("output_human_gdna.csv")
+
+#%% Plotting TNTC only for human gDNA
+
+input_data = pd.read_csv('output_human_gdna.csv')
 
 fig, axes = plt.subplots(figsize=(10, 2.5))
 subset = input_data[input_data['motif seq'] == 'TNTC']
@@ -218,7 +187,7 @@ axes.spines["right"].set_visible(False)
 # Tick marks
 axes.tick_params(axis="both", which="both", bottom=False, left=False)
 # Display yticks with intervals of 1. Alternative oneliner: ax.set_yticks(ax.get_yticks()[::2])
-axes.set_ylim([0, 2])
+axes.set_ylim([0, 3])
 ymin, ymax = axes.get_ylim()
 axes.set_yticks(np.arange(ymin, ymax+1, 1, dtype=np.int)) # Set the values of the ticks
 axes.set_yticklabels(np.arange(ymin, ymax+1, 1, dtype=np.int)) # Set the displayed values of the ticks
@@ -228,15 +197,13 @@ fig.text(0.0001, 0.5, '% gDNA modified', va='center', rotation='vertical') # Com
 #plt.setp(axes[:, 0], ylabel='% gDNA modified') # Apply yaxis label only to plots in column 1
 fig.tight_layout(rect=[0, 0.03, 1, 0.9]) # Call tight_layout last. (left, bottom, right, top)
 
-matplotlib.use("TkAgg") # Backend to use for PyCharm interactive plots
-matplotlib.use("GTK3Agg") # Backend to use for savefig with properly scaled DPI
-fig.savefig("plots/human gDNA - TNTC only", dpi=300)
+
+fig.savefig("tests/human gDNA - TNTC only", dpi=300)
 
 
-# Modification of chromosomes vs length
-max(input_data['record length'])
-min(input_data['record length'])
-# Array of chromosome length in Mb
+#%% Modification of chromosomes vs length
+
+
 np.arange(min(input_data['record length']), max(input_data['record length']), 10e6, dtype=np.int) / 1e6
 
 #linear_reg = LinearRegression()
@@ -272,10 +239,15 @@ fig.text(0.01, 0.5, '% gDNA modified', va='center', rotation='vertical')  # Comm
 fig.tight_layout(rect=[0.02, 0.05, 1, 0.9]) # Call tight_layout last
 
 
-### Comparing gDNA of Taq and human gDNA
+
+#%% Comparing bacterial gDNA with human gDNA - % modified
+
 human_gdna = pd.read_csv("output_human_gdna.csv")
 bac_gdna = pd.read_csv("output.csv")
-np.mean(human_gdna['Perc DNA modified (total)'])
+
+human_gdna_tntc = human_gdna[(human_gdna['motif seq'] == 'TNTC')]
+np.mean(human_gdna_tntc['Perc DNA modified (total)'])
+
 
 # Subset the desired data for bacterial gDNA
 taq_gdna = bac_gdna[(bac_gdna['Record'] == 'T.aquaticus') & (bac_gdna['motif seq'] == 'TNTC')]
@@ -286,7 +258,7 @@ taq_ecol_gdna = bac_gdna[((bac_gdna['Record'] == 'T.aquaticus') | (bac_gdna['Rec
 # Creating new dataset for just Taq and human gDNA
 # Kinda hacky using values[0], but it works for now
 gdna_dict = {'Record': [taq_ecol_gdna['Record'].values[0], taq_ecol_gdna['Record'].values[1], 'H.sapiens'],
-             'Perc DNA modified (total)': [taq_ecol_gdna['Perc DNA modified (total)'].values[0], taq_ecol_gdna['Perc DNA modified (total)'].values[1], np.mean(human_gdna['Perc DNA modified (total)'])]
+             'Perc DNA modified (total)': [taq_ecol_gdna['Perc DNA modified (total)'].values[0], taq_ecol_gdna['Perc DNA modified (total)'].values[1], np.mean(human_gdna_tntc['Perc DNA modified (total)'])]
 }
 
 # Plotting
@@ -310,9 +282,8 @@ fig.text(0.0001, 0.5, '% gDNA modified', va='center', rotation='vertical') # Com
 #plt.setp(axes[:, 0], ylabel='% gDNA modified') # Apply yaxis label only to plots in column 1
 fig.tight_layout(rect=[0, 0.03, 1, 0.9]) # Call tight_layout last. (left, bottom, right, top)
 
-matplotlib.use("TkAgg") # Backend to use for PyCharm interactive plots
-matplotlib.use("GTK3Agg") # Backend to use for savefig with properly scaled DPI
-fig.savefig("plots/Taq, Ecol vs human gDNA - TNTC modification", dpi=300)
+
+fig.savefig("tests/Taq, Ecol vs human gDNA - TNTC modification", dpi=300)
 
 
 
@@ -322,104 +293,25 @@ fig.savefig("plots/Taq, Ecol vs human gDNA - TNTC modification", dpi=300)
 
 
 
-# Calculating position of each DNA motif
-test_seq = 'GATACGATTAAAAATCTCAAAAGCACCAGATCGA'
-test_seq.find('TCTC') # only finds the first occurance
-
-# List comprehension to find index of occurances
-T = [i for i, j in enumerate('TAAT') if j == 'T']
-A = [i for i, j in enumerate('TAAT') if j == 'A']
-
-
-# Add sequence to object
-seq = ''
-for record in SeqIO.parse('k12-gdna.fna', 'fasta'):
-	seq = record.seq
-# Make a smaller, faster processing sequence
-seq_sm = seq[:int(((len(seq))/1000))]
-len(seq_sm)
-
-# Count positions of a single letter
-test_dict = {'A': [i for i, j in enumerate(seq_sm) if j == 'A'],
-             'T': [i for i, j in enumerate(seq_sm) if j == 'T'],
-             'G': [i for i, j in enumerate(seq_sm) if j == 'G'],
-             'C': [i for i, j in enumerate(seq_sm) if j == 'C']
-             }
-
-# OK-ish plot. Unsure if at all useful
-for k, v in test_dict.items():
-	plt.xlim(0, len(seq_sm))
-	sns.kdeplot(test_dict[k], bw=0.02, label=k)
-
-
-# Make 4x subplots for each nucleotide
-# fig, axes = plt.subplots(4, 1)
-# for i, (k, v) in enumerate(test_dict.items()):
-# 	sns.kdeplot(test_dict[k],
-# 	            bw=0.02,
-# 	            ax=i+1
-# 	            )
-# plt.show()
-
-
-fig = plt.figure()
-for i, (k, v) in enumerate(test_dict.items()):
-	ax = fig.add_subplot(4, 1, i+1)
-	plt.setp(ax, xlim=(0, len(seq_sm)), ylim=(0,0.0004))
-	sns.kdeplot(test_dict[k], bw=0.02, ax=ax)
-	ax.set_ylabel(k, fontsize=20)
-#plt.xlim(0, len(seq_sm))
-plt.xlabel('Position (kb)', fontsize=20)
-plt.suptitle('Nucleotide distribution in K12 E. coli', fontsize=25)
-plt.show
 
 
 
 
 
 
-### Extra gDNA plots
-
-mots = ['TNTC', 'TNTM', 'TYT', 'TTTH']
-output = pd.DataFrame()
-for i, _ in enumerate(mots):
-	print("Counting sites for {0} motif".format(mots[i]))
-	df = pd.DataFrame(data=fc.motifs_in_fasta('downloaded_DNA/all-bac-extra-renamed.fa', str(mots[i])))
-	output = output.append(df)
-output.to_csv('output-extra.csv')
-
-input_data = pd.read_csv('output-extra.csv')
-
-max(input_data['Perc DNA modified (total)'])
-
-fig, axes = plt.subplots(nrows=2, ncols=2, sharey=False, sharex=True)
-for i, ax in enumerate(axes.flatten()):
-	subset = input_data[input_data['motif seq'] == str(mots[i])]
-	ax.bar(subset['Record'], subset['Perc DNA modified (total)'], color=plot_colours[i])
-	ax.set_xticklabels(labels=subset['Record'], rotation=45, ha="right", rotation_mode="anchor")
-	ax.set_title('\'{0}\' motif'.format(mots[i]))
-	# Remove plot borders
-	ax.spines["top"].set_visible(False)
-	#ax.spines["bottom"].set_visible(False)
-	ax.spines["right"].set_visible(False)
-	#ax.spines["left"].set_visible(False)
-	# Tick marks
-	ax.get_yaxis().tick_left()
-	ax.tick_params(axis="both", which="both", bottom=False, left=False)
-	# Display yticks with intervals of 1. Alternative oneliner: ax.set_yticks(ax.get_yticks()[::2])
-	ax.set_ylim([0, 6])
-	ymin, ymax = ax.get_ylim()
-	ax.set_yticks(np.arange(ymin, ymax+1, 1, dtype=np.int))
-	ax.set_yticklabels(np.arange(ymin, ymax+1, 1, dtype=np.int))
-	#ax.set_ylabel('% gDNA modified')
-# fig.suptitle('This is the figure title')
-fig.text(0.01, 0.5, '% gDNA modified', va='center', rotation='vertical') # Common Y axis label
-#plt.setp(axes[:, 0], ylabel='% gDNA modified') # Apply yaxis label only to plots in column 1
-fig.tight_layout(rect=[0, 0.03, 1, 0.9]) # Call tight_layout last
 
 
 
-axes.set_ylim([0, 2])
-ymin, ymax = axes.get_ylim()
-axes.set_yticks(np.arange(ymin, ymax+1, 1, dtype=np.int)) # Set the values of the ticks
-axes.set_yticklabels(np.arange(ymin, ymax+1, 1, dtype=np.int)) # Set the displayed values of the ticks
+
+
+
+
+
+
+
+
+
+
+
+
+
