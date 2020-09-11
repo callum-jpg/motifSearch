540258f531617e93324c120c68e71d6af67f1bb9
callum-jpg <23258116+callum-jpg@users.noreply.github.com>
2020-09-11 17:01:29 +0100

    subplotting of bar plots working

--- motifsearch/classmotifs.py
+++ motifsearch/classmotifs.py
@@ -27,6 +27,9 @@ class motifsearch:
     
     def motif_bar(self, motif):
         
+        #if type(motif) is str:
+            
+        
         self.subset = self.motif_data[self.motif_data['motif seq'] == str(motif)]
         self.species = self.subset['Record']
         self.perc = self.subset['Perc DNA modified (total)']
@@ -45,7 +48,36 @@ class motifsearch:
         ax.set_ylim([0, math.ceil(max(self.perc))])
         ymin, ymax = ax.get_ylim()
         ax.yaxis.set_ticks(np.arange(ymin, ymax+1, 1))
-    
+        
+    def motif_bar1(self, motif):
+        
+        if type(motif) is str:
+            motif = [motif]
+        
+        self.rows = math.ceil(len(motif)/2)
+        self.cols = 2
+        
+        fig, ax_ = plt.subplots(self.rows, self.cols, sharey=False, sharex=True)
+        axes = np.array(ax_)
+        for i, ax in enumerate(axes.flatten()):
+            self.subset = self.motif_data[self.motif_data['motif seq'] == str(motif[i])]
+            self.species = self.subset['Record']
+            self.perc = self.subset['Perc DNA modified (total)']
+            ax.bar(self.species, self.perc)
+            # Customise plot
+            ax.set_xticklabels(labels=self.species, rotation=45, ha="right", rotation_mode="anchor")
+            ax.set_title('\'{0}\' motif'.format(motif[i]))
+            # Remove plot borders    
+            ax.spines["top"].set_visible(False)
+            ax.spines["right"].set_visible(False)
+            ax.get_yaxis().tick_left()
+            ax.tick_params(axis="both", which="both", bottom=False, left=False)
+            # Display yticks with intervals of 1. Alternative oneliner: ax.set_yticks(ax.get_yticks()[::2])
+            # Round up to the nearest int for the highest percentage
+            ax.set_ylim([0, math.ceil(max(self.perc))])
+            ymin, ymax = ax.get_ylim()
+            ax.yaxis.set_ticks(np.arange(ymin, ymax+1, 1))    
+        
     
 # Useful code for getting Mb from bp ax.set_yticklabels([x/1000 for x in ax.get_yticks()])
     
--- tests/with classes.py
+++ tests/with classes.py
@@ -26,19 +26,43 @@ mots = ['TNTC', 'TNTM', 'TYT', 'TTTH']
 
 motif = motifsearch()
 
-y = motif.count('downloaded_DNA/bacterial-gDNA-renamed.fa', ['TNTC'])
+y = motif.count('downloaded_DNA/bacterial-gDNA-renamed.fa', mots)
 
-motif.motif_bar('TNTC')
+motif.motif_bar1(mots)
 
 
 #%%
 
-test = '[1, 2, 3]'
+test = mots
 
 if type(test) is str:
-    print("hello")
+    test1 = [test]
+    print('str')
+elif type(test) is list:
+    test1 = [x for x in test]
+    print('list')
 else:
-    print('waaa')
+    print('neither')
+    
+print(test)
+
+#%%
+import matplotlib.pyplot as plt
+
+fig, axes = plt.subplots(nrows=1, ncols=1, sharey=False, sharex=True)
+y = np.array([axes])
+for i, j in enumerate(y):
+    print(i, j)
+    
+    
+#%%
+
+fig, ax = plt.subplots(2, 2, sharey=False, sharex=True)
+axes = np.array(ax)
+for i, axes1, in enumerate(axes.flatten()):
+    print(i, axes1)
+
+
 #%% Theoritical use of classes
 
 # Count motifs
