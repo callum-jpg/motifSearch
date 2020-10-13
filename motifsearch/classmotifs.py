bcf9107b519aae0fd57e7e7e3a4a0f6f53a0aa4a
callum-jpg <23258116+callum-jpg@users.noreply.github.com>
2020-10-13 14:42:12 +0100

    added linear regression method

--- motifsearch/classmotifs.py
+++ motifsearch/classmotifs.py
@@ -4,6 +4,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+from sklearn.linear_model import LinearRegression
+
 
 class motifsearch:
     
@@ -128,7 +130,7 @@ class motifsearch:
     
         fig, ax = plt.subplots(1, 1, sharey=False, sharex=True,
                                 figsize=(figure_width, figure_height))
-        ax.bar(species, dna_lengths, color=self.plot_colours[0])
+        ax.bar(species, dna_lengths, color=self.plot_colours[2])
         # Customise plot
         ax.set_xticklabels(labels=species, rotation=45, ha="right", rotation_mode="anchor")
         # ax.set_title('DNA sequence lengths (Mb)')
@@ -151,6 +153,83 @@ class motifsearch:
         fig.text(figure_width*1e-4, 0.55, 'DNA length (Mb)', va='center', rotation='vertical') # Common Y axis label
         fig.tight_layout(rect=[0, 0.03, 1, 0.9]) # Call tight_layout last
         return fig
+        
+    
+    def count_vs_length(self, input_data, motif, width=None, height=None):
+        linear_reg = LinearRegression()
+        
+        if width != None:
+            figure_width = width
+        else:
+            figure_width = (len(set(input_data['Record'])))
+            
+        if height != None:
+            figure_height = height 
+        else:
+            figure_height = math.ceil(max(input_data['record length (Mb)']  ))*0.8
+            
+            
+        # Round up to calculate number of rows based on motifs
+        rows = math.ceil(len(motif)/2)
+        
+        if len(motif) == 1:
+            cols = 1
+        else:
+            cols = 2
+        
+        fig, ax_ = plt.subplots(rows, cols, sharey=False, sharex=False,
+                                figsize=(figure_width, figure_height))
+        # Make ax_ a 1/2D array, regardless of length
+        axes = np.array(ax_)
+        for i, ax in enumerate(axes.flatten()):
+            # Plot bar only for subplots which has a corresponding motif
+            # Delete unrequired subplot in the else statement
+            if i in range(0, len(motif)):
+                subset = input_data[input_data['motif seq'] == str(motif[i])]
+
+                x = np.reshape(subset['record length (Mb)'].values, (len(subset['record length (Mb)'].values), 1))
+                y = np.reshape(subset['Perc DNA modified (total)'].values, (len(subset['Perc DNA modified (total)'].values), 1))
+                	# Linear regression predict
+                linear_reg.fit(x, y)
+                y_predict = linear_reg.predict(x) # Prediction holder
+                	# Plot
+                ax.scatter(x, y, color=self.plot_colours[i])
+                ax.plot(x, y_predict, color='red')
+
+                # Customise plot
+                #ax.set_xticklabels(labels=self.species, rotation=45, ha="right", rotation_mode="anchor")
+                ax.set_title('\'{0}\' motif'.format(motif[i]))
+                # Remove plot borders    
+                ax.spines["top"].set_visible(False)
+                ax.spines["right"].set_visible(False)
+                ax.get_yaxis().tick_left()
+                ax.tick_params(axis="both", which="both", bottom=False, left=False)
+                # Display yticks with intervals of 1. Alternative oneliner: ax.set_yticks(ax.get_yticks()[::2])
+                # Round up to the nearest int for the highest percentage
+                if max(y) < 1:
+                    # For modifications below 1, find rescale the y axis
+                    upper_bound = np.round(max(y)*1.5, 1)
+                    ax.set_ylim([0, upper_bound])
+                    ymin, ymax = ax.get_ylim()
+                    np.arange(ymin, ymax, upper_bound)
+                else:
+                    ax.set_ylim([0, math.ceil(max(y))])
+                    ymin, ymax = ax.get_ylim()
+                    ax.yaxis.set_ticks(np.arange(ymin, ymax+1, 1))  
+            else:
+                # I don't like this solution for removing a sub plot
+                # But since the nrow/col is fixed at 2 you can assume that, assuming an odd number
+                # of motifs is plotted, that the plot to delete is in the i = -1 and j = 1 of the 
+                # np.array.
+                # Assuming an even grid (2x2, 4x4 etc), this functionality will be ok. 
+                axes[self.rows-1, self.cols-1].remove()
+        # figure_width*1e-4 hopefully scales OK with varying dataset sizes
+        fig.text(figure_width*1e-4, 0.5, '% gDNA modified', va='center', rotation='vertical') # Common Y axis label
+        fig.text(0.5, 0.04, 'DNA length (Mb)', ha='center', va='center')
+        fig.tight_layout(rect=[0, 0.03, 1, 0.9]) # Call tight_layout last
+        return fig
+        
+        
         
     
 # for i, j in enumerate(mots):
