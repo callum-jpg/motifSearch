ed346872022cc15165affbd75e2195a57c5fc1b8
callum-jpg <23258116+callum-jpg@users.noreply.github.com>
2020-10-13 14:48:45 +0100

    tidying and removing overzealous selfs

--- motifsearch/classmotifs.py
+++ motifsearch/classmotifs.py
@@ -40,16 +40,16 @@ class motifsearch:
             motif = [motif]
         
         # Round up to calculate number of rows based on motifs
-        self.rows = math.ceil(len(motif)/2)
+        rows = math.ceil(len(motif)/2)
         
         if len(motif) == 1:
-            self.cols = 1
+            cols = 1
         else:
-            self.cols = 2
+            cols = 2
             
         if width != None:
             figure_width = width
-        elif self.cols == 1:
+        elif cols == 1:
             figure_width = len(set(input_data['Record'])) * 0.5
         else:
             figure_width = (len(set(input_data['Record'])))
@@ -57,20 +57,13 @@ class motifsearch:
         if height != None:
             figure_height = height 
         # Adjust figure height based on row count
-        elif self.rows == 1:
-            figure_height = self.rows*0.5
+        elif rows == 1:
+            figure_height = rows*0.5
         else:
-            figure_height = self.rows*2
+            figure_height = rows*2
+
         
-        # # Colours for plotting. From Solarized palette
-        # colour_palette = [(42, 161, 152), (38, 139, 210), (108, 113, 196), (211, 54, 130)]
-        # plot_colours = colour_palette
-        # for i in range(len(colour_palette)):
-        # 	r, g, b = colour_palette[i]
-        # 	# Convert RGB (0, 255) to (0, 1) which matplotlib likes
-        # 	plot_colours[i] = (r / 255, g / 255, b / 255)
-        
-        fig, ax_ = plt.subplots(self.rows, self.cols, sharey=False, sharex=True,
+        fig, ax_ = plt.subplots(rows, cols, sharey=False, sharex=True,
                                 figsize=(figure_width, figure_height))
         # Make ax_ a 1/2D array, regardless of length
         axes = np.array(ax_)
@@ -78,12 +71,12 @@ class motifsearch:
             # Plot bar only for subplots which has a corresponding motif
             # Delete unrequired subplot in the else statement
             if i in range(0, len(motif)):
-                self.subset = input_data[input_data['motif seq'] == str(motif[i])]
-                self.species = self.subset['Record']
-                self.perc = self.subset['Perc DNA modified (total)']
-                ax.bar(self.species, self.perc, color=self.plot_colours[i])
+                subset = input_data[input_data['motif seq'] == str(motif[i])]
+                species = subset['Record']
+                perc = subset['Perc DNA modified (total)']
+                ax.bar(species, perc, color=self.plot_colours[i])
                 # Customise plot
-                ax.set_xticklabels(labels=self.species, rotation=45, ha="right", rotation_mode="anchor")
+                ax.set_xticklabels(labels=species, rotation=45, ha="right", rotation_mode="anchor")
                 ax.set_title('\'{0}\' motif'.format(motif[i]))
                 # Remove plot borders    
                 ax.spines["top"].set_visible(False)
@@ -92,7 +85,7 @@ class motifsearch:
                 ax.tick_params(axis="both", which="both", bottom=False, left=False)
                 # Display yticks with intervals of 1. Alternative oneliner: ax.set_yticks(ax.get_yticks()[::2])
                 # Round up to the nearest int for the highest percentage
-                ax.set_ylim([0, math.ceil(max(self.perc))])
+                ax.set_ylim([0, math.ceil(max(perc))])
                 ymin, ymax = ax.get_ylim()
                 ax.yaxis.set_ticks(np.arange(ymin, ymax+1, 1))  
             else:
@@ -101,7 +94,7 @@ class motifsearch:
                 # of motifs is plotted, that the plot to delete is in the i = -1 and j = 1 of the 
                 # np.array.
                 # Assuming an even grid (2x2, 4x4 etc), this functionality will be ok. 
-                axes[self.rows-1, self.cols-1].remove()
+                axes[rows-1, cols-1].remove()
         # figure_width*1e-4 hopefully scales OK with varying dataset sizes
         fig.text(figure_width*1e-4, 0.55, '% gDNA modified', va='center', rotation='vertical') # Common Y axis label
         fig.tight_layout(rect=[0, 0.03, 1, 0.9]) # Call tight_layout last
@@ -182,11 +175,10 @@ class motifsearch:
         # Make ax_ a 1/2D array, regardless of length
         axes = np.array(ax_)
         for i, ax in enumerate(axes.flatten()):
-            # Plot bar only for subplots which has a corresponding motif
-            # Delete unrequired subplot in the else statement
-            if i in range(0, len(motif)):
+            if i in range(0, len(motif)): # If i isn't in range, else will remove the subplot
                 subset = input_data[input_data['motif seq'] == str(motif[i])]
-
+                
+                # x, y for linear_reg
                 x = np.reshape(subset['record length (Mb)'].values, (len(subset['record length (Mb)'].values), 1))
                 y = np.reshape(subset['Perc DNA modified (total)'].values, (len(subset['Perc DNA modified (total)'].values), 1))
                 	# Linear regression predict
@@ -197,7 +189,6 @@ class motifsearch:
                 ax.plot(x, y_predict, color='red')
 
                 # Customise plot
-                #ax.set_xticklabels(labels=self.species, rotation=45, ha="right", rotation_mode="anchor")
                 ax.set_title('\'{0}\' motif'.format(motif[i]))
                 # Remove plot borders    
                 ax.spines["top"].set_visible(False)
@@ -221,8 +212,8 @@ class motifsearch:
                 # But since the nrow/col is fixed at 2 you can assume that, assuming an odd number
                 # of motifs is plotted, that the plot to delete is in the i = -1 and j = 1 of the 
                 # np.array.
-                # Assuming an even grid (2x2, 4x4 etc), this functionality will be ok. 
-                axes[self.rows-1, self.cols-1].remove()
+                # Assuming an even grid (2x2, 4x4 etc), this functionality will be ok.        
+                axes[rows-1, cols-1].remove()
         # figure_width*1e-4 hopefully scales OK with varying dataset sizes
         fig.text(figure_width*1e-4, 0.5, '% gDNA modified', va='center', rotation='vertical') # Common Y axis label
         fig.text(0.5, 0.04, 'DNA length (Mb)', ha='center', va='center')
