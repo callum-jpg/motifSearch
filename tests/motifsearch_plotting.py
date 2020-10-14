a36e1f5351765c9aacaaf72eaa683860986515ea
callum-jpg <23258116+callum-jpg@users.noreply.github.com>
2020-10-14 18:06:00 +0100

    removed old code and added some comments

--- tests/motifsearch_plotting.py
+++ tests/motifsearch_plotting.py
@@ -1,29 +1,31 @@
 #!/usr/bin/env python3
-from motifsearch.classmotifs import motifsearch
-
-#%%
-# Counting motifs with classes
-
 from motifsearch import motifSearch
 
-mots = ['TNTC', 'GTCT', 'GGATC', 'CCTA']
-#mots = ['GATC']
+#%%
+# Counting motifs for the bacterial gDNA downloaded with downloadeding_dna.py
 
+# Define motifs
+mots = ['TNTC', 'GTCT', 'GGATC', 'CCTA']
+
+# Instantiate motifsearch
 motif = motifSearch()
 
+# Count motifs in FASTA file
 motif_df = motif.count('bacterial-gDNA-renamed.fa', mots)
 
-#motif_df.to_csv('bac_counts.csv')
+# Save motif df to file
+motif_df.to_csv('bac_counts.csv')
 
-#motif_df = pd.read_csv('bac_counts.csv')
+# Read saved df from file
+# motif_df = pd.read_csv('bac_counts.csv')
 
-motif_barplot = motif.motif_bar(motif_df, mots)
+# Plot motif modification burder bar plot for all motifs
+motif.motif_bar(motif_df, mots)
 
+# Plot genome length for species in Mb
 motif.motif_bar_lengths(motif_df)
 
-
-#%% Linear regression
-
+# Linear regression to compare relationship between modification burden and genome length
 motif.count_vs_length(motif_df, mots)
 
 
@@ -36,28 +38,28 @@ motif.motif_bar_lengths(motif_df).savefig('img/motifsearch-bar-plot-lengths', bb
 motif.count_vs_length(motif_df, mots).savefig('img/motifsearch-lin-reg', bbox_inches = 'tight', dpi=300)
 
 
-#%%
 
-# Save the figure
-motif_barplot.savefig("hello", dpi=300)
 
 
 #%%
 # Counting motifs within human autosomes individually
 
-from motifsearch.classmotifs import motifsearch
+from motifsearch import motifSearch
 from natsort import index_natsorted
 import numpy as np
 import pandas as pd
 
 mots = ['TNTC']
 
-motif = motifsearch()
+motif = motifSearch()
 
+# Count motifs in autosomes. Slow to run
 #human_counts = motif.count('downloaded_DNA/human-gdna/1-22-hs-gdna-renamed.fa', mots)
 
+# Save human motif frequency df
 # human_counts.to_csv('human_counts.csv')
 
+# Read human motif frequency df
 human_counts = pd.read_csv('human_counts.csv')
 
 ## Order 'chr1:22' into their natural order
@@ -70,20 +72,17 @@ human_counts = pd.read_csv('human_counts.csv')
 # https://stackoverflow.com/questions/8966538/syntax-behind-sortedkey-lambda
 human_counts_sorted = human_counts.sort_values(by='Record', key=lambda x: np.argsort(index_natsorted(human_counts['Record'])))
 
-human_plot = motif.motif_bar(human_counts_sorted, mots)
+# Plot motif frequency for each chromosome
+motif.motif_bar(human_counts_sorted, mots)
 
-#%% SEM of autosome modification
+#%% Determine SEM of autosome modification
 import numpy as np
 
 human_std = np.std(human_counts['Perc DNA modified (total)'])
 
 print(human_std)
 
-#%%
-
-human_counts = pd.read_csv('human_counts.csv')
-
-#%% 
+#%% Comparing bacterial motif frequency with human
 
 
 human_counts = pd.read_csv('human_counts.csv')
@@ -105,24 +104,26 @@ hs_data = {
 # Save dict as dataframe
 pd.DataFrame.from_dict(data=hs_data, orient='columns').to_csv('human_counts_summary.csv')
 
-
+# Extract data for only E. coli and T. acquaticus for TNTC motif
 taq_ecol_gdna = bac_counts[((bac_counts['Record'] == 'T.aquaticus') 
                             | (bac_counts['Record'] == 'E.coli(K12)'))
                     & (bac_counts['motif seq'] == 'TNTC')]
 
-
+# Read consolidated human motif data
 hs_gdna = pd.read_csv('human_counts_summary.csv')
 
+# Concatenate bacterial with human data and save
 taq_ecol_gdna.append(hs_gdna).to_csv('taq_ecol_human_gdna_counts.csv')
 
+# Read bacterial-human motif frequency data
 taq_ecol_human = pd.read_csv('taq_ecol_human_gdna_counts.csv')
 
-
-motif = motifsearch()
+# Plot for TNTC motif
+motif = motifSearch()
 motif.motif_bar(taq_ecol_human, 'TNTC')
 
 
-#%%
+#%% Adjust dimensions of the plot and save
 
 plot = motif.motif_bar(taq_ecol_human, 'TNTC', width=2, height=3)
 
@@ -134,47 +135,3 @@ plot.savefig("taq_ecol_human_counts.png", dpi=300)
 plot = motif.motif_bar_lengths(taq_ecol_human, 2, 3)
 
 plot.savefig("taq_ecol_human_lengths.png", dpi=300)
-
-#%%
-
-unique = ['T.acq', 'H.sap', 'M.tub', 'H.sap', 'M.tub']
-
-# Return unique values from list
-print(set(unique))
-
-# Return number of rows, columns
-print(y.shape)
-
-print(len(set(y['Record'])))
-
-# print(human_counts['Record'])
-# print(sorted(human_counts['Record']))
-
-
-
-    
-#%%
-
-print(y[['Record', 'Perc DNA modified (total)', 'motif seq']])
-
-
-#%% Theoritical use of classes
-
-# Count motifs
-# Have count method return pd.df
-motif_df = motifs.count(mots)
-
-# Plot the motif df
-motifs.plot_perc(motif_df)
-
-# Plot gDNA lengths
-motifs.plot_length(motif_df)
-
-# Plot length vs perc modified
-motifs.plot_length_mod(motif_df, regression = T)
-
-
-
-
-
-
