a6228bdc753bfbb79ec42a06724cbe01bd66522c
callum-jpg <23258116+callum-jpg@users.noreply.github.com>
2020-10-14 17:22:32 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='motifSearch',
+      version='1.0',
+      description='For counting motifs in genomic DNA',
+      author='Callum T-C',
+      author_email='email@example.com',
+      url='https://github.com/callum-jpg/motifSearch',
+      install_requires=[
+      'biopython==1.77',
+      'numpy>=1.18.1',
+      'pandas==1.1.2',
+      'scikit-learn==0.23.2',
+      'matplotlib==3.1.3'
+      ]
+     )
